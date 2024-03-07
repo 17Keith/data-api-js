458c81e3f0726137f57275bdd2faa31e0049100e
17Keith <kayomega7@gmail.com>
2024-03-07 07:49:12 +0300

    U

--- Tabular-Data/tab.js
+++ Tabular-Data/tab.js
@@ -1,10 +1,20 @@
 // Attempting to work with csv files in JS. The file is already 'cleaned' and there are no missing values in it.
 
-getData()
+tabData();
 
-async function getData() {
-    const response = await fetch('test.csv')
-    const data = await response.text()
+async function tabData() {
+    const response = await fetch('ZonAnn.Ts+dSST.csv');
+    const data = await response.text();
 
-    console.log(data)
+    const table = data.split('\n').slice(1);
+    // console.log(data)
+
+    table.forEach(row => {
+
+        const columns = row.split(',');
+        const year = columns[0];
+        const temp = columns[1];
+        console.log(year, temp);
+    });
+
 }
