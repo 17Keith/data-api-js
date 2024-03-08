8cd30e9216e090a9511f02d5afc39f95c3fe7e87
17Keith <kayomega7@gmail.com>
2024-03-08 22:29:26 +0300

    Load up the starter files and set up Chart.js

---
+++ Chart/chart.js
@@ -0,0 +1,20 @@
+// Displaying the dataset using Chart.js.
+
+tabData();
+
+async function tabData() {
+    const response = await fetch('ZonAnn.Ts+dSST.csv');
+    const data = await response.text();
+
+    const table = data.split('\n').slice(1);
+    // console.log(data)
+
+    table.forEach(row => {
+
+        const columns = row.split(',');
+        const year = columns[0];
+        const temp = columns[1];
+        console.log(year, temp);
+    });
+
+}
