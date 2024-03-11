c31c47971e0fbc31f12d35f6008bf8140abdea5b
17Keith <kayomega7@gmail.com>
2024-03-11 13:08:39 +0300

    Chart the data

--- Chart/chart.js
+++ Chart/chart.js
@@ -1,8 +1,45 @@
 // Displaying the dataset using Chart.js.
 
-tabData();
+chartData();
+
+async function chartData() {
+    const data = await tabData();
+    const ctx = document.getElementById("myChart");
+
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: data.xlabels,
+            datasets: [
+                {
+                    label: "Global Average Temperature Difference in °C",
+                    data: data.ytemps,
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    ticks: {
+                        // Include a degree sign in the ticks
+                        callback: function(value, index, ticks) {
+                            return value + '°';
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+
 
 async function tabData() {
+
+    const xlabels = [];
+    const ytemps = [];
+
     const response = await fetch('ZonAnn.Ts+dSST.csv');
     const data = await response.text();
 
@@ -13,8 +50,10 @@ async function tabData() {
 
         const columns = row.split(',');
         const year = columns[0];
+        xlabels.push(year);
         const temp = columns[1];
+        ytemps.push(parseFloat(temp) + 14);
         console.log(year, temp);
     });
-
+    return { xlabels, ytemps };
 }
